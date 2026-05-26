3df0690f5abdb419788814baff5a2099e8b1b7fa
Avichal Kaushik <90247880+avi-kaushik@users.noreply.github.com>
2026-05-26 05:59:37 +0000

    Java program to insert at the end, delete the head & delete kth node of a linked list.

--- programs/java/linkedlists/basic/ManageNodesInCircularLinkedList.java
+++ programs/java/linkedlists/basic/ManageNodesInCircularLinkedList.java
@@ -71,6 +71,101 @@ public class ManageNodesInCircularLinkedList {
         return newNode;
     }
 
+    /**
+     * Insert node at end using only head pointer.
+     */
+    public static Node<Integer> insertAtEnd(Node<Integer> head, int n) {
+
+        // Create new node
+        Node<Integer> newNode = new Node<Integer>(n);
+
+        // Handle empty circular linked list
+        if (head == null) {
+
+            newNode.next = newNode;
+
+        } else {
+
+            // Insert new node after head
+            newNode.next = head.next;
+            head.next = newNode;
+
+            // Swap data between head and new node
+            int temp = newNode.data;
+            newNode.data = head.data;
+            head.data = temp;
+        }
+
+        // Return updated tail node
+        return newNode;
+    }
+
+    /**
+     * Delete head node from circular linked list.
+     */
+    public static Node<Integer> deleteHead(Node<Integer> head) {
+
+        // Return if circular linked list is empty
+        if (head == null)
+            return head;
+
+        // Return null if only one node exists
+        if (head.next == head)
+            return null;
+
+        // Copy next node data into head
+        head.data = head.next.data;
+
+        // Remove next node
+        head.next = head.next.next;
+
+        // Return updated head
+        return head;
+    }
+
+    /**
+     * Delete node at given position in circular linked list.
+     *
+     * Note:
+     * -----
+     * This implementation does not validate position bounds.
+     *
+     * If position is greater than linked list length,
+     * it may throw NullPointerException.
+     *
+     * Time Complexity:
+     * ----------------
+     * O(n)
+     *
+     * Space Complexity:
+     * -----------------
+     * O(1)
+     */
+    public static Node<Integer> deleteNode(Node<Integer> head, int position) {
+
+        // Return if circular linked list is empty
+        if (head == null)
+            return head;
+
+        // Delete head node
+        if (position == 1)
+            return deleteHead(head);
+
+        // Start traversal from head
+        Node<Integer> current = head;
+
+        // Traverse to previous node
+        for (int i = 1; i < position - 1; i++) {
+            current = current.next;
+        }
+
+        // Remove target node
+        current.next = current.next.next;
+
+        // Return updated head
+        return head;
+    }
+
     public static void main(String[] args) {
 
         Node<Integer> head = new Node<>(4);
@@ -86,9 +181,43 @@ public class ManageNodesInCircularLinkedList {
         // Last node points back to head
         tail.next = head;
 
+        System.out.println("Initial Circular Linked List:");
+        TraverseCircularLinkedList.print(head);
+
+        System.out.println();
+
         // Insert node at beginning
+        System.out.println("Insert 12 at beginning");
         head = insertAtBeginning(head, 12);
 
+        System.out.println("After insertion at beginning:");
+        TraverseCircularLinkedList.print(head);
+
+        System.out.println();
+
+        // Insert node at end
+        System.out.println("Insert 9 at end");
+        head = insertAtEnd(head, 9);
+
+        System.out.println("After insertion at end:");
+        TraverseCircularLinkedList.print(head);
+
+        System.out.println();
+
+        // Delete head node
+        System.out.println("Delete head node");
+        head = deleteHead(head);
+
+        System.out.println("After deleting head:");
+        TraverseCircularLinkedList.print(head);
+
+        System.out.println();
+
+        // Delete node at position 3
+        System.out.println("Delete node at position 3");
+        head = deleteNode(head, 3);
+
+        System.out.println("After deleting node at position 3:");
         TraverseCircularLinkedList.print(head);
     }
 }
