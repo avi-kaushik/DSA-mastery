a581e25cdef547edcf884092d5b80442a7e262f4
Avichal Kaushik <90247880+avi-kaushik@users.noreply.github.com>
2026-07-28 21:05:14 +0000

    Java program to get factorial & nth fibonacci number using recursion.

--- programs/java/techniques/recursion/basic/RecursionPractice.java
+++ programs/java/techniques/recursion/basic/RecursionPractice.java
@@ -3,6 +3,51 @@ package programs.java.techniques.recursion.basic;
 public class RecursionPractice {
 
     /**
+     * Returns the nth Fibonacci number using recursion.
+     *
+     * @param n The position in the Fibonacci sequence.
+     * @return The nth Fibonacci number.
+     * @throws IllegalArgumentException If n is negative.
+     */
+    public static int getFibonacci(int n) {
+        // Validate the input.
+        // Fibonacci numbers are not defined for negative values.
+        if (n < 0)
+            throw new IllegalArgumentException("n cannot be negative.");
+
+        // Base case:
+        // The 0th Fibonacci number is 0.
+        if (n == 0)
+            return 0;
+
+        // Base case:
+        // The 1st Fibonacci number is 1.
+        if (n == 1)
+            return 1;
+
+        // Recursively calculate the current Fibonacci number
+        // by summing the previous two Fibonacci numbers.
+        return getFibonacci(n - 1) + getFibonacci(n - 2);
+    }
+
+    /**
+     * Returns the factorial of a given number using recursion.
+     *
+     * @param n The number whose factorial is to be calculated.
+     * @return The factorial of n.
+     */
+    public static int getFactorial(int n) {
+        // Base case:
+        // The factorial of 0 is defined as 1.
+        if (n == 0)
+            return 1;
+
+        // Recursively multiply the current number
+        // with the factorial of the previous number.
+        return n * getFactorial(n - 1);
+    }
+
+    /**
      * Prints numbers from 1 to n using recursion.
      *
      * @param n The upper limit of the sequence.
@@ -46,5 +91,8 @@ public class RecursionPractice {
         System.out.println();
 
         printNTo1(4);
+
+        System.out.println("Factorial of 5: " + getFactorial(5));
+        System.out.println("7th Fibonacci: " + getFibonacci(7));
     }
 }
